0de1084cf3b0d80e8ee95ff1b369488f5d60105b
manios krasanakis <maniospas@hotmail.com>
2024-02-03 02:12:49 +0200

    added a metric wrapper

---
+++ test/test.py
@@ -0,0 +1,18 @@
+from mammoth.datasets import CSV
+from mammoth.models import ONNX
+from mammoth.exports import Markdown
+from typing import Dict, List
+from mammoth.integration import metric
+
+
+@metric(version="v003", python="3.11")
+def new_metric(
+    dataset: CSV,
+    model: ONNX,
+    sensitive: List[str],
+    branch: str = "all",
+) -> Markdown:
+    """Write your metric's description here.
+    """
+    return Markdown("This is a test")
+
