60780310b246e8adb49e29f9755ac6d125573ac1
manios krasanakis <maniospas@hotmail.com>
2024-03-01 15:00:55 +0200

    correct none handling for kfp

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt", "r") as file:
 
 setuptools.setup(
     name='MAMMOth-commons',
-    version='0.0.11',
+    version='0.0.12',
     author="Emmanouil (Manios) Krasanakis",
     author_email="maniospas@hotmail.com",
     description="Component interfaces of the MAMMOth fairness toolkit.",
