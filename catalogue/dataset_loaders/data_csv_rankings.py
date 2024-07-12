f3f78f28ba44531413d2471f3e698c784597ae98
Giorgos Nomikos <gnom@exus.ai>
2024-07-12 08:39:39 +0300

    Default value for Path so component can be built.

--- catalogue/dataset_loaders/data_csv_rankings.py
+++ catalogue/dataset_loaders/data_csv_rankings.py
@@ -7,9 +7,9 @@ import pandas as pd
 from typing import List, Optional
 
 
-@loader(namespace="mammotheu", version="v003", python="3.11")
+@loader(namespace="mammotheu", version="v0020", python="3.11")
 def data_csv_rankings(
-    path: str,
+    path: str = "",
     numeric: list =["Ranking", "Value"],  # numeric = ["age", "duration", "campaign", "pdays", "previous"]
     categorical: list = ["Gender", "Nationality"],  # ["job", "marital", "education", "default", "housing", "loan", "contact", "poutcome",]
     labels: Optional[str] = None,
