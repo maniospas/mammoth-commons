d46c6fb504a8f67bdfdfb3c5c8c75989a3ea7418
maniospas <maniospas@hotmail.com>
2024-07-15 22:49:10 +0300

    fixed missing dataset param in metric

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt", "r") as file:
 
 setuptools.setup(
     name="MAMMOth-commons",
-    version="0.0.18",
+    version="0.0.19",
     author="Emmanouil (Manios) Krasanakis",
     author_email="maniospas@hotmail.com",
     description="Component interfaces of the MAMMOth fairness toolkit.",
