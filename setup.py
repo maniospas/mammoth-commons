1ea06ff5db2b00ea3f8aedc6d2fca2b53889a9bb
maniospas <maniospas@hotmail.com>
2024-10-23 00:10:18 +0300

    prevent coverage badge checks on pull requests

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("requirements[test].txt", "r") as file:
 
 setuptools.setup(
     name="MAMMOth-commons",
-    version="0.0.29",
+    version="0.0.30",
     author="Emmanouil (Manios) Krasanakis",
     author_email="maniospas@hotmail.com",
     description="Component interfaces of the MAMMOth fairness toolkit.",
