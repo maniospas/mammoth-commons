d9c5cd1fa8074e80946e09f573dcdff78c72c98f
maniospas <maniospas@hotmail.com>
2024-10-23 14:42:16 +0300

    Added facex description

--- catalogue/metrics/xai_analysis.py
+++ catalogue/metrics/xai_analysis.py
@@ -21,7 +21,7 @@ def facex(
     target_class: int = None,
     target_layer: str = None,
 ) -> HTML:
-    """Write your metric's description here.
+    """Analyses 19 facial regions and accessories to provide explanations.
 
     Args:
         target_class: The integer identifier of the target class.
@@ -30,5 +30,10 @@ def facex(
 
     target_class = int(target_class)
     html = run_mammoth(dataset, sensitive[0], target_class, model.model, target_layer)
-
+    html = """
+    FaceX analysed 19 facial regions and accessories to provide explanations. In the two illustrations below,
+    left are face regions and right are hat and glasses. Blue are the least important regions and red the most
+    important ones that are taken into account. Based on the outputs, try to the question of “where does a model
+    focus on?”. We also show high-impact patches to help understand “what visual features trigger its focus?”.
+    """ + html
     return HTML(html)
