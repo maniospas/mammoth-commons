91075e0a11cf2c5f7d3aaf20a2ce0547325c3e2b
Arjun Roy <34624153+arjunroyihrpa@users.noreply.github.com>
2024-11-04 12:09:18 +0100

    Add files via upload

--- mammoth/datasets/csv.py
+++ mammoth/datasets/csv.py
@@ -10,9 +10,17 @@ def _features(df, numeric, categorical):
     ]
     return pd.concat(dfs, axis=1).values
 
+def _pred_features(df, numeric, categorical, sensitives):
+    import pandas as pd
+
+    dfs = [df[col] for col in numeric if col not in sensitives] + [
+        pd.get_dummies(df[col]) for col in categorical if col not in sensitives
+    ]
+    return pd.concat(dfs, axis=1).values
+
 
 class CSV(Dataset):
-    def __init__(self, data, numeric, categorical, labels):
+    def __init__(self, data, numeric, categorical, labels, sensitives=None):
         import pandas as pd
 
         self.data = data
@@ -20,6 +28,10 @@ class CSV(Dataset):
         self.categorical = categorical
         self.labels = pd.get_dummies(data[labels])
         self.cols = numeric + categorical
+        if sensitives!=None:
+            self.pred_cols=[col for col in self.cols if col not in sensitives]
+        else: 
+            self.pred_cols=[col for col in self.cols]
 
     def to_features(self, sensitive):
         """for attr in sensitive:
@@ -28,3 +40,12 @@ class CSV(Dataset):
                 "Fairness analysis on CSV datasets is not supported for non-categorical sensitive attributes."
             )"""
         return _features(self.data, self.numeric, self.categorical).astype(np.float64)
+
+    def to_pred(self, sensitive):
+        """for attr in sensitive:
+        if attr not in self.categorical:
+            raise Exception(
+                "Fairness analysis on CSV datasets is not supported for non-categorical sensitive attributes."
+            )"""
+        return _pred_features(self.data, self.numeric, self.categorical,sensitive).astype(np.float64)
+
