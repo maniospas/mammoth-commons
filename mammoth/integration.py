bd992d109a7b3e07ec4319aa8acee65f596347a9
maniospas <maniospas@hotmail.com>
2024-12-06 11:02:11 +0200

    added pickle to integration

--- mammoth/integration.py
+++ mammoth/integration.py
@@ -2,6 +2,8 @@
 import inspect
 from typing import get_type_hints, Dict, List, get_origin, get_args, Union
 import os
+import pickle
+
 
 _default_python = "3.11"
 _default_packages = ()  # appended to ["mammoth-commons[deployment]"]
